775f5fd7cbd07ee467d219c48e3ed23a5ca7ef3f
Rafael Santana <aguiar.rafael003@gmail.com>
2026-05-20 11:19:57 -0300

    Multiplos de 7 entre dois numeros informados usando for

---
+++ lista03/ex001A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Faça um programa que solicite dois números inteiros positivos e exiba os múltiplos de 7 existentes
+// entre estes números. Faça uma versão com cada um dos laços: for, while e do-while.
+
+int main () {
+    int numeroA, numeroB;
+
+    cout << "Digite dois numeros inteiros positivos: " << endl;
+    cout << "Numero: ";
+    cin >> numeroA;
+    cout << "Numero: ";
+    cin >> numeroB;
+
+    if (numeroB > numeroA) {
+        for (int i = numeroA; i <= numeroB; i++) {
+            if (i % 7 == 0) {
+                cout << i << " ";
+            }
+        }
+    }
+
+    else {
+        for (int i = numeroB; i <= numeroA; i++) {
+            if (i % 7 == 0) {
+                cout << i << " ";
+            }
+        }
+    }
+
+return 0;
+}
