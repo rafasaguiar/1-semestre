b05552b2387327154a7ba48ccc0c3086466b5f5e
Rafael Santana <aguiar.rafael003@gmail.com>
2026-05-20 11:15:34 -0300

    Multiplos de 7 entre dois numeros informados usando while

---
+++ lista03/ex001.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Faça um programa que solicite dois números inteiros positivos e exiba os múltiplos de 7 existentes
+// entre estes números. Faça uma versão com cada um dos laços: for, while e do-while.
+
+int main () {
+    int numeroA, numeroB;
+    int contador = 1;
+
+    cout << "Digite dois numeros inteiros positivos: " << endl;
+    cout << "Numero: ";
+    cin >> numeroA;
+    cout << "Numero: ";
+    cin >> numeroB;
+
+    if (numeroB > numeroA) {
+        contador = numeroA;
+        while (contador <= numeroB) {
+            if (contador % 7 == 0) {
+                cout << contador << " ";
+            }
+            contador++;
+        }
+    }
+
+    else {
+        contador = numeroB;
+        while (contador <= numeroA) {
+            if (contador % 7 == 0) {
+                cout << contador << " ";
+            }
+            contador++;
+        }
+    }
+
+return 0;
+}
