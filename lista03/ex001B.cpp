b7cddfc69791af711b1e55048dacb0e9fcdb6cd8
Rafael Santana <aguiar.rafael003@gmail.com>
2026-05-20 11:36:07 -0300

    Multiplos de 7 entre dois numero informados usando laço do...while

---
+++ lista03/ex001B.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// Faça um programa que solicite dois números inteiros positivos e exiba os múltiplos de 7 existentes
+// entre estes números. Faça uma versão com cada um dos laços: for, while e do-while.
+
+int main () {
+    int numeroA, numeroB;
+    int contador = 1;
+
+    cout << "Digite dois numeros inteiros positivos: " << endl;
+    cout << "Numero: ";
+    cin >> numeroA;
+
+    while (numeroA < 0) {
+        cout << "Digite um valor positivo: ";
+        cin >> numeroA;
+    }
+
+    cout << "Numero: ";
+    cin >> numeroB;
+
+    while (numeroB < 0) {
+        cout << "Digite um valor positivo: ";
+        cin >> numeroB;
+    }
+
+    if (numeroB > numeroA) {
+        contador = numeroA;
+        do {
+            if (contador % 7 == 0) {
+                cout << contador << " ";
+            }
+            contador++;
+        } while (contador <= numeroB);
+    }
+
+    else {
+        contador = numeroB;
+        do {
+            if (contador % 7 == 0) {
+                cout << contador << " ";
+            }
+            contador++;
+        } while (contador <= numeroA);
+    }
+
+return 0;
+}
